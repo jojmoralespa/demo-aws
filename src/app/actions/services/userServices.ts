312f4de2e44b79d4f8e972b311943b5cd79c54c4
Johan Javier Morales Parrado (Amarilo) <johan.morales@amarilo.com>
2024-04-04 22:50:58 -0500

    creation of crud table

---
+++ src/app/actions/services/userServices.ts
@@ -0,0 +1,21 @@
+"use server"
+
+export async function getProducts() {
+    "use server"
+    console.log("Lista de productos")
+}
+
+export async function createProduct() {
+    "use server"
+    console.log("Producto creado")
+}
+
+export async function updateProduct() {
+    "use server"
+    console.log("Producto actualizado")
+}
+
+export async function deleteProduct() {
+    "use server"
+    console.log("Producto borrado")
+}
